87394f9322a1ec8d25b0295a4f92e0d8de60ee05
marcus <marcmonk@gmail.com>
2025-07-04 15:10:42 -0400

    adds a11y and disables telemetry

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,19 @@
 /** @type { import('@storybook/svelte-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
+  stories: ['../src/components/**/*.stories.js'],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   staticDirs: ["../public"],
   framework: {
     name: "@storybook/svelte-vite",
     options: {},
   },
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
+  }
 };
 export default config;
