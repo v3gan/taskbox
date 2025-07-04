cd14a4205e785979940426c68023966ef34276b9
marcus <marcmonk@gmail.com>
2025-07-04 15:12:22 -0400

    completes task component

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -1,32 +1,74 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
-    
-    const dispatch = createEventDispatcher();
+  import { createEventDispatcher } from 'svelte';
 
-    /** Event handler for the Pin Task */
-    function pinTask() {
-        dispatch('onPinTask', {
-            id: task.id,
-        });
-    }
+  const dispatch = createEventDispatcher();
 
-    /** Event handler for the Archive Task */
-    function archiveTask() {
-        dispatch('onArchiveTask', {
-            id: task.id,
-        });
-    }
+  /** Event handler for the Pin Task */
+  function pinTask() {
+    dispatch('onPinTask', {
+      id: task.id,
+    });
+  }
 
-    /** Composition of the task*/
-    export let task = {
-        id: '',
-        title: '',
-        state: '',
-    };
+  /** Event handler for the Archive Task */
+  function archiveTask() {
+    dispatch('onArchiveTask', {
+      id: task.id,
+    });
+  }
+
+  /** Composition of the task*/
+  export let task = {
+    id: '',
+    title: '',
+    state: '',
+  };
+
+  /* Reactive declaration (computed prop in other frameworks) */
+  $: isChecked = task.state === 'TASK_ARCHIVED';
 </script>
 
-<div class="list-item">
-    <label for="title" aria-label={task.title}>
-        <input type="text" value={task.title} name="title" readonly />
-    </label>
+<div class="list-item {task.state}">
+  <label
+    for={`checked-${task.id}`}
+    class="checkbox"
+    aria-label={`archiveTask-${task.id}`}
+  >
+    <input
+      type="checkbox"
+      checked={isChecked}
+      disabled
+      value={task.title}
+      name={`checked-${task.id}`}
+      id={`archiveTask-${task.id}`}
+    />
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <span
+      class="checkbox-custom"
+      role="button"
+      on:click={archiveTask}
+      tabindex="-1"
+      aria-label={`archiveTask-${task.id}`}
+    />
+  </label>
+  <label for={`title-${task.id}`} aria-label={task.title} class="title">
+    <input
+      type="text"
+      value={task.title}
+      name="title"
+      id={`title-${task.id}`}
+      readonly
+      placeholder="Input title"
+    />
+  </label>
+  {#if task.state !== 'TASK_ARCHIVED'}
+    <button
+      class="pin-button"
+      on:click|preventDefault={pinTask}
+      aria-label={`pinTask-${task.id}`}
+      id={`pinTask-${task.id}`}
+    >
+      <span class="icon-star" />
+    </button>
+  {/if}
 </div>
